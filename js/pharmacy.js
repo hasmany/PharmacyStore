969df67dbfeaf91c79d5a902dcfb8813550cc8e0
Hubert Nguyen <hubertanguyen@gmail.com>
2015-04-21 12:52:47 -0500

    added js file for image animation

---
+++ js/pharmacy.js
@@ -0,0 +1,14 @@
+// Flip animation
+
+$(document).ready(function(){
+
+  (function flip_pharamcy () {
+    $(".flip-img").flip();
+    $(".front").css("height","150px");
+    $(".front").css("width","85%");
+
+    $(".back").css("height","150px");
+    $(".back").css("width","85%");
+  })();
+
+});
