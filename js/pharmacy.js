43a4bdad235b2ef0fa5a2ed937f0902fcdc8f8a3
Hubert Nguyen <hubertanguyen@gmail.com>
2015-04-23 00:22:44 -0500

    Change banner location to center of Carousel, new animation for Carousel

--- js/pharmacy.js
+++ js/pharmacy.js
@@ -2,7 +2,6 @@
 
 $(document).ready(function(){
 
-
   // Flip animation
   (function flip_pharamcy () {
     $(".flip-img").flip();
@@ -65,7 +64,7 @@ $(document).ready(function(){
     }
   })();
 
-  // Carousel
+  // Carousel function
   jssor_slider1_starter = function (containerId) {
     var _CaptionTransitions = [];
     _CaptionTransitions["L"] = { $Duration: 900, x: 0.6, $Easing: { $Left: $JssorEasing$.$EaseInOutSine }, $Opacity: 2 };
@@ -80,11 +79,56 @@ $(document).ready(function(){
     _CaptionTransitions["MCLIP|L"] = { $Duration: 900, $Clip: 1, $Move: true, $Easing: { $Clip: $JssorEasing$.$EaseInOutCubic} };
     _CaptionTransitions["MCLIP|R"] = { $Duration: 900, $Clip: 2, $Move: true, $Easing: { $Clip: $JssorEasing$.$EaseInOutCubic} };
 
-    // Options for Carousel, adjust values for variations
+    // Carousel Slide Animation list
+    var _SlideshowTransitions = [
+    //Collapse Random
+    { $Duration: 1000, $Delay: 80, $Cols: 10, $Rows: 4, $Clip: 15, $SlideOut: true, $Easing: $JssorEasing$.$EaseOutQuad }
+
+    //Fade in LR Chess
+    , { $Duration: 1200, y: 0.3, $Cols: 2, $During: { $Top: [0.3, 0.7] }, $ChessMode: { $Column: 12 }, $Easing: { $Top: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2, $Outside: true }
+
+    //Rotate VDouble+ out
+    , { $Duration: 1000, x: -1, y: 2, $Rows: 2, $Zoom: 11, $Rotate: 1, $SlideOut: true, $Assembly: 2049, $ChessMode: { $Row: 15 }, $Easing: { $Left: $JssorEasing$.$EaseInExpo, $Top: $JssorEasing$.$EaseInExpo, $Zoom: $JssorEasing$.$EaseInExpo, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInExpo }, $Opacity: 2, $Round: { $Rotate: 0.85 } }
+
+    //Zoom HDouble+ out
+    , { $Duration: 1200, x: 4, $Cols: 2, $Zoom: 11, $SlideOut: true, $Assembly: 2049, $ChessMode: { $Column: 15 }, $Easing: { $Left: $JssorEasing$.$EaseInExpo, $Zoom: $JssorEasing$.$EaseInExpo, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
+
+    //Rotate Zoom+ out BL
+    , { $Duration: 1200, x: 4, y: -4, $Zoom: 11, $Rotate: 1, $SlideOut: true, $Easing: { $Left: $JssorEasing$.$EaseInExpo, $Top: $JssorEasing$.$EaseInExpo, $Zoom: $JssorEasing$.$EaseInExpo, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInExpo }, $Opacity: 2, $Round: { $Rotate: 0.8 } }
+
+    //Dodge Dance Inside in Random
+    , { $Duration: 1500, x: 0.3, y: -0.3, $Delay: 80, $Cols: 10, $Rows: 4, $Clip: 15, $During: { $Left: [0.3, 0.7], $Top: [0.3, 0.7] }, $Easing: { $Left: $JssorEasing$.$EaseInJump, $Top: $JssorEasing$.$EaseInJump, $Clip: $JssorEasing$.$EaseOutQuad }, $Round: { $Left: 0.8, $Top: 2.5 } }
+
+    //Rotate VFork+ out
+    , { $Duration: 1200, x: -3, y: 1, $Rows: 2, $Zoom: 11, $Rotate: 1, $SlideOut: true, $Assembly: 2049, $ChessMode: { $Row: 28 }, $Easing: { $Left: $JssorEasing$.$EaseInExpo, $Top: $JssorEasing$.$EaseInExpo, $Zoom: $JssorEasing$.$EaseInExpo, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInExpo }, $Opacity: 2, $Round: { $Rotate: 0.7 } }
+
+    //Clip and Chess in
+    , { $Duration: 1200, y: -1, $Cols: 10, $Rows: 4, $Clip: 15, $During: { $Top: [0.5, 0.5], $Clip: [0, 0.5] }, $Formation: $JssorSlideshowFormations$.$FormationStraight, $ChessMode: { $Column: 12 }, $ScaleClip: 0.5 }
+
+    //Rotate Zoom- out TL
+    , { $Duration: 1200, x: 0.5, y: 0.5, $Zoom: 1, $Rotate: 1, $SlideOut: true, $Easing: { $Left: $JssorEasing$.$EaseInCubic, $Top: $JssorEasing$.$EaseInCubic, $Zoom: $JssorEasing$.$EaseInCubic, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseInCubic }, $Opacity: 2, $Round: { $Rotate: 0.5 } }
+
+    //Rotate Zoom- in BR
+    , { $Duration: 1200, x: -0.6, y: -0.6, $Zoom: 1, $Rotate: 1, $During: { $Left: [0.2, 0.8], $Top: [0.2, 0.8], $Zoom: [0.2, 0.8], $Rotate: [0.2, 0.8] }, $Easing: { $Zoom: $JssorEasing$.$EaseSwing, $Opacity: $JssorEasing$.$EaseLinear, $Rotate: $JssorEasing$.$EaseSwing }, $Opacity: 2, $Round: { $Rotate: 0.5 } }
+
+    // Wave out Eagle
+    , { $Duration: 1500, y: -0.5, $Delay: 60, $Cols: 24, $SlideOut: true, $Formation: $JssorSlideshowFormations$.$FormationCircle, $Easing: $JssorEasing$.$EaseInWave, $Round: { $Top: 1.5 } }
+
+    //Expand Stairs
+    , { $Duration: 1000, $Delay: 30, $Cols: 10, $Rows: 4, $Clip: 15, $Formation: $JssorSlideshowFormations$.$FormationStraightStairs, $Assembly: 2050, $Easing: $JssorEasing$.$EaseInQuad }
+
+    //Fade Clip out H
+    , { $Duration: 1200, $Delay: 20, $Clip: 3, $SlideOut: true, $Assembly: 260, $Easing: { $Clip: $JssorEasing$.$EaseOutCubic, $Opacity: $JssorEasing$.$EaseLinear }, $Opacity: 2 }
+
+    ////Dodge Pet Inside in Random Chess
+    //, { $Duration: 1500, x: 0.2, y: -0.1, $Delay: 80, $Cols: 10, $Rows: 4, $Clip: 15, $During: { $Left: [0.2, 0.8], $Top: [0.2, 0.8] }, $ChessMode: { $Column: 15, $Row: 15 }, $Easing: { $Left: $JssorEasing$.$EaseInWave, $Top: $JssorEasing$.$EaseInWave, $Clip: $JssorEasing$.$EaseLinear }, $Round: { $Left: 0.8, $Top: 2.5} }
+    ];
+
+    // Options paramter for Carousel
     var options = {
       $FillMode: 2,
       $AutoPlay: true,
-      $AutoPlayInterval: 3000,
+      $AutoPlayInterval: 2500,
       $PauseOnHover: 1,
       $ArrowKeyNavigation: true,
       $SlideEasing: $JssorEasing$.$EaseOutQuint,
@@ -120,14 +164,20 @@ $(document).ready(function(){
         $ChanceToShow: 1,
         $AutoCenter: 2,
         $Steps: 1
-      }
+      },
+
+      $SlideshowOptions: {
+        $Class: $JssorSlideshowRunner$,
+        $Transitions: _SlideshowTransitions,
+        $TransitionsOrder: 1,
+        $ShowLink: true
+      },
     };
 
     // Instantiate Carousel
     var jssor_slider1 = new $JssorSlider$(containerId, options);
 
-    // Response function
-
+    // Response function, for Carousel
     function ScaleSlider() {
       var bodyWidth = document.body.clientWidth;
       if (bodyWidth)
@@ -147,7 +197,7 @@ $(document).ready(function(){
   // Carousel Trigger, DOM id is the parameter
   jssor_slider1_starter('slider1_container');
 
-  // Nav slide function
+  // Nav page slide function
   $(function() {
     $('a[href*=#]:not([href=#myModal])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
